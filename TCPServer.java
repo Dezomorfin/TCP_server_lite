404c4f5724f9a26656aaa1bb8d53ca3c4e1d39f0
Dr.Vaggo <39808040+Dezomorfin@users.noreply.github.com>
2018-12-22 18:30:41 +0400

    Add files via upload

---
+++ TCPServer.java
@@ -0,0 +1,30 @@
+package tcpserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+
+    public static void main(String[] args) {
+        
+        try(ServerSocket sServer = new ServerSocket(1000);
+                Socket cn = sServer.accept();
+                BufferedReader bis = new BufferedReader(new InputStreamReader(cn.getInputStream()));)
+        {
+            String line = bis.readLine();
+            
+            while (line != null && !line.equals("")) {
+                    System.out.println(line);
+                    line = bis.readLine();
+            }
+            
+        } catch (IOException ex) {
+            System.out.println("Error in connection: " + ex.getMessage());
+        }
+        
+    }
+    
+}
